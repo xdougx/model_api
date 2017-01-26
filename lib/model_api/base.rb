07ba29d157abe82124ff7ff0df526b82618780cc
Douglas Rossignolli <douglas@kanamobi.com.br>
2017-01-26 16:08:33 -0200

    fix

--- lib/model_api/base.rb
+++ lib/model_api/base.rb
@@ -85,10 +85,11 @@ module ModelApi
 
       def define_attributes(*attributes)
         class_eval do
+          with_timestamp = attributes.clone
           attr_accessor *attributes.delete_if {|attr| attr.match /created_at|updated_at/ }
 
           define_method(:attributes) do
-            @attributes ||= attributes.each_with_object({}) do |attr, attrs|
+            @attributes ||= with_timestamp.each_with_object({}) do |attr, attrs|
               attrs[attr] = send(attr)
             end
           end
