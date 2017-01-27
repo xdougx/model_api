4c22b13d3c5de15f758d6b200d9d0a742fa668bf
Douglas Rossignolli <douglas@kanamobi.com.br>
2017-01-27 10:10:30 -0200

    fix relations

--- lib/model_api/relations.rb
+++ lib/model_api/relations.rb
@@ -1,37 +1,37 @@
+# Relations module will build up attr_acessor for relations
 module Relations
-
+  # Relations module will build up attr_acessor for relations
   module ClassMethods
     def belongs_to(relation_name, options = {})
-      klass = get_relation_class(options)
+      klass = get_relation_class(relation_name, options)
       
       class_eval do
         define_setter(relation_name, klass)
         define_getter(relation_name, klass)
       end
+    end
 
-      private 
+    private 
 
-      def define_setter(relation_name, klass)
+    def define_setter(relation_name, klass)
         define_method("#{relation_name}=", args) do
           instance_variable_set(relation_name, klass.build(args))
         end
-      end
+    end
 
-      def define_getter(relation_name, klass)
-        define_method(relation_name) do
-          instance_variable_get(:"@#{relation_name}") || klass.new
-        end
+    def define_getter(relation_name, klass)
+      define_method(relation_name) do
+        instance_variable_get(:"@#{relation_name}") || klass.new
       end
+    end
 
-      def get_relation_class(options = {})
-        klass = options[:class_name] ? options[:class_name] : relation_name.camelize
-        defined?(klass) ? klass.constantize : raise_class_not_defined(klass)
-      end
+    def get_relation_class(relations_name, options = {})
+      klass = options[:class_name] ? options[:class_name] : relation_name.camelize
+      defined?(klass) ? klass.constantize : raise_class_not_defined(klass)
+    end
 
-      def raise_class_not_defined(klass)
-        fail "#{klass} isn't defined"
-      end
-
+    def raise_class_not_defined(klass)
+      fail "#{klass} isn't defined"
     end
   end
 end
